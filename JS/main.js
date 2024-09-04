1c487f35cbcce6182efb68c86ba7f5634bd5e55e
Ricardo Ferreira <ricardoferreiran@gmail.com>
2024-09-05 01:14:44 +0200

    commit antes de los media queries

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+// Efecto typing
+
 const div = document.querySelector(".text"),
       texto = "Bienvenido a mi portfolio de Frontend Developer!!";
 
@@ -7,8 +9,20 @@ function efectotyping(elemento, texto, i = 0){
      if(i === texto.length -1){
         return
     }
-    setTimeout(() => efectotyping(div, texto, i + 1), 100);
 
+    setTimeout(() => efectotyping(div, texto, i + 1), 100);
 }
+ 
 
 efectotyping(div, texto)
+
+
+// Clase is active del menu
+
+document.querySelectorAll('.menu li a').forEach(link => {
+        link.addEventListener('click', function() {
+            document.querySelector('.menu li a.is-active')?.classList.remove('is-active');
+            this.classList.add('is-active');
+        });
+});
+
